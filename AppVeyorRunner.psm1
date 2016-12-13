150460532d2ccd2be0de3b5c99b5391481108290
Michael Greene <michael.greene@microsoft.com>
2016-12-13 09:33:50 -0600

    share env var across functions

--- AppVeyorRunner.psm1
+++ AppVeyorRunner.psm1
@@ -2,12 +2,15 @@
 Comments
 #>
 
+$env:ResourceGroupName = 'TestAutomation'+$env:APPVEYOR_PULL_REQUEST_NUMBER
+$env:AutomationAccountName = 'DSCValidation'+$env:APPVEYOR_PULL_REQUEST_NUMBER
+
 <##>
 function New-ResourceGroupforTests {
     param(
         [string]$Location = 'EastUS2',
-        [string]$ResourceGroupName = 'TestAutomation'+$env:APPVEYOR_PULL_REQUEST_NUMBER,
-        [string]$AutomationAccountName = 'DSCValidation'+$env:APPVEYOR_PULL_REQUEST_NUMBER
+        [string]$ResourceGroupName = $env:ResourceGroupName,
+        [string]$AutomationAccountName = $env:AutomationAccountName
     )
     try {
         # Create Resource Group
@@ -31,13 +34,13 @@ function New-ResourceGroupforTests {
 <##>
 function Remove-AzureTestResources {
     param(
-        [string]$ResourceGroupName = 'TestAutomation'+$env:APPVEYOR_PULL_REQUEST_NUMBER
+        [string]$ResourceGroupName = $env:ResourceGroupName
     )
     try {
         $Remove = Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
     }
     catch [System.Exception] {
-        throw "A failure occured while removing the Resource Group or Auatomation Account`n$error"
+        throw "An error occured while removing the Resource Group or Auatomation Account`n$error"
     }
 }
 
@@ -45,20 +48,25 @@ function Remove-AzureTestResources {
 function Import-ModulesToAzureAutomation {
     param(
         [array]$Modules,
-        [string]$ResourceGroupName = $env:APPVEYOR_BUILD_ID,
-        [string]$AutomationAccountName = $env:APPVEYOR_PROJECT_NAME
+        [string]$ResourceGroupName = $env:ResourceGroupName,
+        [string]$AutomationAccountName = $env:AutomationAccountName
     )
-    # Upload required DSC resources (required modules)
-    $ImportedModules = @()
-    foreach($AutomationModule in $Modules) {
-        $ImportedModules += New-AzureRMAutomationModule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $AutomationModule.Name -ContentLink $AutomationModule.URI
-    }
-
-    # The resource modules must finish the "Creating" stage before the configuration will compile successfully
-    foreach ($ImportedModule in $ImportedModules) {
-        while ((Get-AzureRMAutomationModule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ImportedModule.Name).ProvisioningState -ne 'Succeeded') {
-        Start-Sleep -Seconds 15
+    try {
+        # Upload required DSC resources (required modules)
+        $ImportedModules = @()
+        foreach($AutomationModule in $Modules) {
+            $ImportedModules += New-AzureRMAutomationModule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $AutomationModule.Name -ContentLink $AutomationModule.URI
         }
+
+        # The resource modules must finish the "Creating" stage before the configuration will compile successfully
+        foreach ($ImportedModule in $ImportedModules) {
+            while ((Get-AzureRMAutomationModule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ImportedModule.Name).ProvisioningState -ne 'Succeeded') {
+            Start-Sleep -Seconds 15
+            }
+        }   
+    }
+    catch [System.Exception] {
+        throw "An error occured while importing the modules to Azure Automation`n$error"
     }
 }
 
@@ -66,23 +74,28 @@ function Import-ModulesToAzureAutomation {
 function Import-ConfigurationToAzureAutomation {
     param(
         [psobject]$Configuration,
-        [string]$ResourceGroupName = $env:APPVEYOR_BUILD_ID,
-        [string]$AutomationAccountName = $env:APPVEYOR_PROJECT_NAME
+        [string]$ResourceGroupName = $env:ResourceGroupName,
+        [string]$AutomationAccountName = $env:AutomationAccountName
     )
-    # Import Configuration to Azure Automation DSC
-    $ConfigurationImport = Import-AzureRmAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -SourcePath $Configuration.Location -Published -Force
+    try {
+            # Import Configuration to Azure Automation DSC
+            $ConfigurationImport = Import-AzureRmAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -SourcePath $Configuration.Location -Published -Force
 
-    # Load configdata if it exists
-    if (Test-Path ".\ConfigurationData\$($Configuration.Name).ConfigData.psd1") {
-        $ConfigurationData = Import-PowerShellDataFile ".\ConfigurationData\$($Configuration.Name).ConfigData.psd1"
-    }
+            # Load configdata if it exists
+            if (Test-Path ".\ConfigurationData\$($Configuration.Name).ConfigData.psd1") {
+                $ConfigurationData = Import-PowerShellDataFile ".\ConfigurationData\$($Configuration.Name).ConfigData.psd1"
+            }
 
-    # Splate params to compile in Azure Automation DSC
-    $CompileParams = @{
-    ResourceGroupName     = $ResourceGroupName
-    AutomationAccountName = $AutomationAccountName
-    ConfigurationName     = $Configuration.Name
-    ConfigurationData     = $ConfigurationData
+            # Splate params to compile in Azure Automation DSC
+            $CompileParams = @{
+            ResourceGroupName     = $ResourceGroupName
+            AutomationAccountName = $AutomationAccountName
+            ConfigurationName     = $Configuration.Name
+            ConfigurationData     = $ConfigurationData
+        }
+        $Compile = Start-AzureRmAutomationDscCompilationJob @CompileParams
     }
-    $Compile = Start-AzureRmAutomationDscCompilationJob @CompileParams
+    catch [System.Exception] {
+        throw "An error occured while importing or compiling the configurations using Azure Automation`n$error"        
+    }
 }
