6e6839f5b7631968706c706e88f176b3c9e42a29
Michael Greene <michael.greene@microsoft.com>
2016-12-14 16:14:10 -0600

    add some manifest tests

--- DSCConfiguration.Unit.Tests.ps1
+++ DSCConfiguration.Unit.Tests.ps1
@@ -4,14 +4,23 @@ Describe 'Universal configuration tests' {
     Context 'Module properties' {
         $Name = Get-Item -Path .\ | ForEach-Object -Process {$_.Name}
         $Files = Get-ChildItem
-        It 'Contains a module file' {
+        It 'Contains a module file that aligns to the folder name' {
             $Files.Name.Contains("$Name.psm1") | Should Be True
         }
-        It 'Contains a module manifest' {
+        It 'Contains a module manifest that aligns to the folder and module names' {
             $Files.Name.Contains("$Name.psd1") | Should Be True
         }
         It 'Contains a readme' {
             $Files.Name.Contains('README.md') | Should Be True
+        }
+        It 'Mainfest should import as a data file' {
+            $Manifest = Import-PowerShellDataFile -Path $Name.psd1 | Should Not Throw
+        }
+        It 'Should point to the root module in the manifest' {
+            $Manifest.RootModule | Should Be ".\$Name.psm1"
+        }
+        It 'Should have a GUID in the manifest' {
+            $Mainfest.GUID | Should Match '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
         }
     }
 }
