f19ed4b60f84e595017d595c7d413dd38ef02fe9
Michael Greene <michael.greene@microsoft.com>
2017-01-02 14:39:31 -0600

    correct param input

--- DSCConfiguration.Unit.Tests.ps1
+++ DSCConfiguration.Unit.Tests.ps1
@@ -72,7 +72,7 @@ Describe 'Common Tests - PS Script Analyzer' {
 
         Context $Psm1File.Name {
             It 'Should pass all error-level PS Script Analyzer rules' {
-                $errorPssaRulesOutput = Invoke-ScriptAnalyzer @invokeScriptAnalyzerParameters -ExcludeRule $knownPssaRuleNames -Severity 'Error'
+                $errorPssaRulesOutput = Invoke-ScriptAnalyzer @invokeScriptAnalyzerParameters -ExcludeRule $ignorePssaRuleNames -Severity 'Error'
 
                 if ($null -ne $errorPssaRulesOutput) {
                     Write-Warning -Message 'Error-level PSSA rule(s) did not pass.'
