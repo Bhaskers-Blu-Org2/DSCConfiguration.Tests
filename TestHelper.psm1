4ec18280d1e5958a719fe1e30e68e686ad096893
Michael Greene <michael.greene@microsoft.com>
2017-01-02 14:16:26 -0600

    additional function

--- TestHelper.psm1
+++ TestHelper.psm1
@@ -257,3 +257,28 @@ function Get-SuppressedPSSARuleNameList
 
     return $suppressedPSSARuleNames
 }
+
+<#
+    .SYNOPSIS
+        Tests if a file is encoded in Unicode.
+
+    .PARAMETER FileInfo
+        The file to test.
+#>
+function Test-FileInUnicode
+{
+    [OutputType([Boolean])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [System.IO.FileInfo]
+        $FileInfo
+    )
+
+    $filePath = $FileInfo.FullName
+    $fileBytes = [System.IO.File]::ReadAllBytes($filePath)
+    $zeroBytes = @( $fileBytes -eq 0 )
+
+    return ($zeroBytes.Length -ne 0)
+}
