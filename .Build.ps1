90b9c9e984ef6177eb842b92071e6c0c89db1e77
Michael Greene <michael.greene@microsoft.com>
2017-01-05 15:40:10 -0600

    verify the issue is not line breaks

--- .Build.ps1
+++ .Build.ps1
@@ -218,18 +218,7 @@ task AzureVM {
             $dnsLabelPrefix = "$(Get-Random -Minimum 1000 -Maximum 9999) `
             $($testConfiguration.Name.substring(0,10))"
 
-            New-AzureRmResourceGroupDeployment -Name $BuildID -ResourceGroupName `
-            "TestAutomation$BuildID" `
-            -TemplateFile $BuildFolder\DSCConfiguration.Tests\AzureDeploy.json `
-            -TemplateParameterFile `
-            $BuildFolder\DSCConfiguration.Tests\AzureDeploy.parameters.json `
-            -dnsLabelPrefix $dnsLabelPrefix `
-            -vmName $testConfiguration
-            -adminPassword $adminPassword `
-            -registrationUrl $registrationUrl `
-            -registrationKey $registrationKey `
-            -nodeConfigurationName $testConfiguration.localhost `
-            -verbose
+            New-AzureRMResourceGroupDeployment -Name $BuildID -ResourceGroupName "TestAutomation$BuildID" -TemplateFile "$BuildFolder\DSCConfiguration.Tests\AzureDeploy.json" -TemplateParameterFile "$BuildFolder\DSCConfiguration.Tests\AzureDeploy.parameters.json" -dnsLabelPrefix $dnsLabelPrefix -vmName $testConfiguration -adminPassword $adminPassword -registrationUrl $registrationUrl -registrationKey $registrationKey -nodeConfigurationName $testConfiguration.localhost -verbose
         }
         Write-Task AzureVM -End
     }
