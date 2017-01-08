2e5cf37f7605cc3855ea97c77f3ed4e52af3de1e
Michael Greene <michael.greene@microsoft.com>
2017-01-08 10:19:14 -0600

    output build info outside job

--- .Build.ps1
+++ .Build.ps1
@@ -152,6 +152,7 @@ task AzureVM {
     $VMDeployments = @()
     ForEach ($Configuration in $script:Configurations) {
       ForEach ($WindowsOSVersion in $Configuration.WindowsOSVersion) {
+        Write-Output "Deploying build $BuildID of configuration $Configuration to OS version $WindowsOSVersion"
         $VMDeployment = Start-Job -ScriptBlock {New-AzureTestVM} -ArgumentList @{
             BuildID = $BuildID
             Configuration = $Configuration.Name
